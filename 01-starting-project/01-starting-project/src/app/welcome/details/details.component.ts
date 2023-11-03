751cd5dbbf83cdff9b3b2319d8b48003f9dbe995
sakshimote <sakshimote19@gmail.com>
2023-11-03 15:57:24 +0530

    new commit

--- 01-starting-project/01-starting-project/src/app/welcome/details/details.component.ts
+++ 01-starting-project/01-starting-project/src/app/welcome/details/details.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { AnalyticsService } from 'src/app/shared/analytics.service';
+import { HighlightDirective } from 'src/app/shared/highlight.directive';
+
 
 @Component({
+  standalone:true,
+  imports:[HighlightDirective],
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.css'],
+  providers:[AnalyticsService]
 })
 export class DetailsComponent {
   constructor(private analyticsService: AnalyticsService) {}
--- 01-starting-project/01-starting-project/src/main.ts
+++ 01-starting-project/01-starting-project/src/main.ts
@@ -1,12 +1,18 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { AnalyticsService } from './app/shared/analytics.service';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+// platformBrowserDynamic().bootstrapModule(AppModule)
+//   .catch(err => console.error(err));
+
+bootstrapApplication(AppComponent,{
+  providers:[AnalyticsService]
+});
